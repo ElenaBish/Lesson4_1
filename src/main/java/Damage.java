569fef70c47c3ebe57254e9eaf06f70748bf16d0
ElenaBish <elbish@mail.ru>
2017-05-02 17:06:30 +0600

    Initial commit

---
+++ src/main/java/Damage.java
@@ -0,0 +1,7 @@
+/**
+ * Created by elena on 02.05.2017.
+ */
+public class Damage {
+    private  int sizeOfDamage;
+
+}
---
+++ src/main/java/ServiceCity.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by elena on 02.05.2017.
+ */
+public class ServiceCity {
+    private Map<String, CityStreet> streetMap = new HashMap<>();
+    private Map<String, CityPark> parkMap=new HashMap<>();
+
+}
